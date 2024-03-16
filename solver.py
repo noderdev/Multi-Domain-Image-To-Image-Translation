4c4c162e348a2ba5916a579b6f7b56fd44a1f7ec
Saurav Singh <singhsaurav538@gmail.com>
2024-03-16 14:33:46 -0400

    Add files via upload

---
+++ solver.py
@@ -0,0 +1,366 @@
+from model import Generator
+from model import Discriminator
+from torch.autograd import Variable
+from torchvision.utils import save_image
+import torch
+import torch.nn.functional as F
+import numpy as np
+import os
+import time
+import datetime
+
+
+class Solver(object):
+    def __init__(self, celeba_loader, rafd_loader, config):
+
+        # Data loader.
+        self.celeba_loader = celeba_loader
+        self.rafd_loader = rafd_loader
+
+        # Model configs.
+        self.c_dim = config.c_dim
+        self.c2_dim = config.c2_dim
+        self.image_size = config.image_size
+        self.g_conv_dim = config.g_conv_dim
+        self.d_conv_dim = config.d_conv_dim
+        self.g_repeat_num = config.g_repeat_num
+        self.d_repeat_num = config.d_repeat_num
+        self.lambda_cls = config.lambda_cls
+        self.lambda_rec = config.lambda_rec
+        self.lambda_gp = config.lambda_gp
+
+        # Directories.
+        self.log_dir = config.log_dir
+        self.sample_dir = config.sample_dir
+        self.model_save_dir = config.model_save_dir
+        self.result_dir = config.result_dir
+        self.result_sections = config.result_sections
+
+        # Training configs.
+        self.dataset = config.dataset
+        self.batch_size = config.batch_size
+        self.num_iters = config.num_iters
+        self.num_iters_decay = config.num_iters_decay
+        self.g_lr = config.g_lr
+        self.d_lr = config.d_lr
+        self.n_critic = config.n_critic
+        self.beta1 = config.beta1
+        self.beta2 = config.beta2
+        self.resume_iters = config.resume_iters
+        self.selected_attrs = config.selected_attrs
+
+        # Test configs.
+        self.test_iterations = config.test_iters
+
+        # Miscellaneous.
+        self.use_tensorboard = config.use_tensorboard
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        # Step size.
+        self.logging_step = config.log_step
+        self.sample_step = config.sample_step
+        self.model_save_step = config.model_save_step
+        self.lr_update_step = config.lr_update_step
+
+        # Build the model and tensorboard.
+        self.build_model()
+        if self.use_tensorboard:
+            self.build_tensorboard()
+
+    def build_model(self):
+
+        if self.dataset in ['CelebA', 'RaFD']:
+            self.G = Generator(self.g_conv_dim, self.c_dim, self.g_repeat_num)
+            self.D = Discriminator(self.image_size, self.d_conv_dim, self.c_dim, self.d_repeat_num) 
+        elif self.dataset in ['Both']:
+            self.G = Generator(self.g_conv_dim, self.c_dim+self.c2_dim+2, self.g_repeat_num)   # 2 for mask vector.
+            self.D = Discriminator(self.image_size, self.d_conv_dim, self.c_dim+self.c2_dim, self.d_repeat_num)
+
+        self.g_optimizer = torch.optim.Adam(self.G.parameters(), self.g_lr, [self.beta1, self.beta2])
+        self.d_optimizer = torch.optim.Adam(self.D.parameters(), self.d_lr, [self.beta1, self.beta2])
+        self.display_network(self.G, 'G')
+        self.display_network(self.D, 'D')
+            
+        self.G.to(self.device)
+        self.D.to(self.device)
+
+    def build_tensorboard(self):
+        """Build a tensorboard logger."""
+        from logger import Logger
+        self.logger = Logger(self.log_dir)
+
+    def display_network(self, model, name):
+        num_params = 0
+        for p in model.parameters():
+            num_params += p.numel()
+        print(model)
+        print(name)
+        print("The number of parameters: {}".format(num_params))
+
+    def restore_model(self, resume_iters):
+        print('Loading the trained models from step {}...'.format(resume_iters))
+        G_path = os.path.join(self.model_save_dir, '{}-G.ckpt'.format(resume_iters))
+        D_path = os.path.join(self.model_save_dir, '{}-D.ckpt'.format(resume_iters))
+        self.G.load_state_dict(torch.load(G_path, map_location=lambda storage, loc: storage))
+        self.D.load_state_dict(torch.load(D_path, map_location=lambda storage, loc: storage))
+
+
+
+    def update_learningRate(self, g_lr, d_lr):
+        for param_group in self.g_optimizer.param_groups:
+            param_group['lr'] = g_lr
+        for param_group in self.d_optimizer.param_groups:
+            param_group['lr'] = d_lr
+
+    def reset_gradient(self):
+        self.g_optimizer.zero_grad()
+        self.d_optimizer.zero_grad()
+
+    def denormalize(self, z):
+        out = (z + 1) / 2
+        return out.clamp_(0, 1)
+
+    def gradient_penalty(self, y, x):
+        """Compute gradient penalty: (L2_norm(dy/dx) - 1)**2."""
+        weight = torch.ones(y.size()).to(self.device)
+        dydx = torch.autograd.grad(outputs=y,
+                                   inputs=x,
+                                   grad_outputs=weight,
+                                   retain_graph=True,
+                                   create_graph=True,
+                                   only_inputs=True)[0]
+
+        dydx = dydx.view(dydx.size(0), -1)
+        dydx_l2norm = torch.sqrt(torch.sum(dydx**2, dim=1))
+        return torch.mean((dydx_l2norm-1)**2)
+
+    def onehot_encode_labels(self, labels, dim):
+        batch_size = labels.size(0)
+        out = torch.zeros(batch_size, dim)
+        out[np.arange(batch_size), labels.long()] = 1
+        return out
+
+    def find_labels(self, c_org, c_dim=5, dataset='CelebA', selected_attrs=None):
+        
+        if dataset == 'CelebA':
+            hair_color_indices = []
+            for i, attr_name in enumerate(selected_attrs):
+                if attr_name in ['Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Gray_Hair']:
+                    hair_color_indices.append(i)
+
+        c_trg_list = []
+        for i in range(c_dim):
+            if dataset == 'CelebA':
+                c_trg = c_org.clone()
+                if i in hair_color_indices:  # Set one hair color to 1 and the rest to 0.
+                    c_trg[:, i] = 1
+                    for j in hair_color_indices:
+                        if j != i:
+                            c_trg[:, j] = 0
+                else:
+                    c_trg[:, i] = (c_trg[:, i] == 0)  # Reverse attribute value.
+            elif dataset == 'RaFD':
+                c_trg = self.onehot_encode_labels(torch.ones(c_org.size(0))*i, c_dim)
+
+            c_trg_list.append(c_trg.to(self.device))
+        return c_trg_list
+
+    def compute_loss(self, logit, target, dataset='CelebA'):
+        if dataset == 'CelebA':
+            return F.binary_cross_entropy_with_logits(logit, target, size_average=False) / logit.size(0)
+        elif dataset == 'RaFD':
+            return F.cross_entropy(logit, target)
+
+    def train(self):
+        # Set data loader.
+        if self.dataset == 'CelebA':
+            data_loader = self.celeba_loader
+        elif self.dataset == 'RaFD':
+            data_loader = self.rafd_loader
+
+        # Fetch fixed inputs for debugging.
+        data_iter = iter(data_loader)
+        x_fixed, c_org = next(data_iter)
+        x_fixed = x_fixed.to(self.device)
+        c_fixed_list = self.find_labels(c_org, self.c_dim, self.dataset, self.selected_attrs)
+
+        # Learning rate cache for decaying.
+        g_lr = self.g_lr
+        d_lr = self.d_lr
+
+        # Start training from scratch or resume training.
+        start_iters = 0
+        if self.resume_iters:
+            start_iters = self.resume_iters
+            self.restore_model(self.resume_iters)
+
+        print('Start training...')
+        start_time = time.time()
+        for i in range(start_iters, self.num_iters):
+
+            # get input images and their labels.
+            try:
+                x_real, label_org = next(data_iter)
+            except:
+                data_iter = iter(data_loader)
+                x_real, label_org = next(data_iter)
+
+            rand_idx = torch.randperm(label_org.size(0))
+            label_trg = label_org[rand_idx]
+
+            if self.dataset == 'CelebA':
+                c_org = label_org.clone()
+                c_trg = label_trg.clone()
+            elif self.dataset == 'RaFD':
+                c_org = self.onehot_encode_labels(label_org, self.c_dim)
+                c_trg = self.onehot_encode_labels(label_trg, self.c_dim)
+
+            x_real = x_real.to(self.device)          
+            c_org = c_org.to(self.device)            
+            c_trg = c_trg.to(self.device)            
+            label_org = label_org.to(self.device)     
+            label_trg = label_trg.to(self.device)     
+
+           
+            ##### Train  discriminator #####
+
+            # Loss real images.
+            out_src, out_class = self.D(x_real)
+            d_loss_real = - torch.mean(out_src)
+            d_loss_cls = self.compute_loss(out_class, label_org, self.dataset)
+
+            # Loss with fake images.
+            fake_img = self.G(x_real, c_trg)
+            out_src, out_cls = self.D(fake_img.detach())
+            d_loss_fake = torch.mean(out_src)
+
+            #Loss for Gradient
+            alpha = torch.rand(x_real.size(0), 1, 1, 1).to(self.device)
+            x_hat = (alpha * x_real.data + (1 - alpha) * fake_img.data).requires_grad_(True)
+            out_src, _ = self.D(x_hat)
+            d_loss_gp = self.gradient_penalty(out_src, x_hat)
+
+            # Back prop
+            d_loss = d_loss_real + d_loss_fake + self.lambda_cls * d_loss_cls + self.lambda_gp * d_loss_gp
+            self.reset_gradient()
+
+            d_loss.backward()
+            self.d_optimizer.step()
+
+            # Logging for tensorboard.
+            loss = {}
+            loss['D/loss_real'] = d_loss_real.item()
+            loss['D/loss_fake'] = d_loss_fake.item()
+            loss['D/loss_cls'] = d_loss_cls.item()
+            loss['D/loss_gp'] = d_loss_gp.item()
+            
+            #  Train the generator  #
+            if (i+1) % self.n_critic == 0:
+                # Original-to-target domain.
+                fake_img = self.G(x_real, c_trg)
+                out_src, out_cls = self.D(fake_img)
+                g_loss_fake = - torch.mean(out_src)
+                g_loss_cls = self.compute_loss(out_cls, label_trg, self.dataset)
+
+                # Reconstruction Loss
+                reconst_img = self.G(fake_img, c_org)
+                g_loss_rec = torch.mean(torch.abs(x_real - reconst_img))
+
+                # Backprop
+                g_loss = g_loss_fake + self.lambda_rec * g_loss_rec + self.lambda_cls * g_loss_cls
+                self.reset_gradient()
+                g_loss.backward()
+                self.g_optimizer.step()
+
+                # Logging for tensorboard.
+                loss['G/loss_fake'] = g_loss_fake.item()
+                loss['G/loss_rec'] = g_loss_rec.item()
+                loss['G/loss_cls'] = g_loss_cls.item()
+
+
+            # Print out training information.
+            if (i+1) % self.logging_step == 0:
+                et = time.time() - start_time
+                et = str(datetime.timedelta(seconds=et))[:-7]
+                log = "Elapsed [{}], Iteration [{}/{}]".format(et, i+1, self.num_iters)
+                for tag, value in loss.items():
+                    log += ", {}: {:.4f}".format(tag, value)
+                print(log)
+
+                if self.use_tensorboard:
+                    for tag, value in loss.items():
+                        self.logger.scalar_summary(tag, value, i+1)
+
+            # Translate fixed images for debugging.
+            if (i+1) % self.sample_step == 0:
+                with torch.no_grad():
+                    x_fake_list = [x_fixed]
+                    for c_fixed in c_fixed_list:
+                        x_fake_list.append(self.G(x_fixed, c_fixed))
+                    x_concat = torch.cat(x_fake_list, dim=3)
+                    sample_path = os.path.join(self.sample_dir, '{}-images.jpg'.format(i+1))
+                    save_image(self.denormalize(x_concat.data.cpu()), sample_path, nrow=1, padding=0)
+                    print('Saved real and fake images into {}...'.format(sample_path))
+
+            # Save model checkpoints.
+            if (i+1) % self.model_save_step == 0:
+                G_path = os.path.join(self.model_save_dir, '{}-G.ckpt'.format(i+1))
+                D_path = os.path.join(self.model_save_dir, '{}-D.ckpt'.format(i+1))
+                torch.save(self.G.state_dict(), G_path)
+                torch.save(self.D.state_dict(), D_path)
+                print('Saved model checkpoints into {}...'.format(self.model_save_dir))
+
+            # Decay learning rates.
+            if (i+1) % self.lr_update_step == 0 and (i+1) > (self.num_iters - self.num_iters_decay):
+                g_lr -= (self.g_lr / float(self.num_iters_decay))
+                d_lr -= (self.d_lr / float(self.num_iters_decay))
+                self.update_learningRate(g_lr, d_lr)
+                print ('Decayed learning rates, g_lr: {}, d_lr: {}.'.format(g_lr, d_lr))
+
+
+    def test(self):
+        # Load the trained generator.
+        self.restore_model(self.test_iterations)
+        
+        # Set data loader.
+        if self.dataset == 'CelebA':
+            data_loader = self.celeba_loader
+        elif self.dataset == 'RaFD':
+            data_loader = self.rafd_loader
+            
+        if not os.path.exists(self.result_sections):
+            os.makedirs(self.result_sections)
+        
+        with torch.no_grad():
+            for i, (x_real, c_org) in enumerate(data_loader):
+
+                # Prepare input images and target domain labels.
+                x_real = x_real.to(self.device)
+                c_trg_list = self.find_labels(c_org, self.c_dim, self.dataset, self.selected_attrs)
+                
+                labelType = 0
+                # Translate images.
+                x_fake_list = [x_real]
+                for c_trg in c_trg_list:
+                    gen_image = self.G(x_real, c_trg)
+                    x_fake_list.append(gen_image)
+
+                    # Save the generated images
+                    j=0
+                    for gen_img in gen_image:
+                        target_dir = os.path.join(self.result_sections,'{}-image-{}.jpg'.format(labelType,j))
+                        save_image(self.denormalize(gen_img),target_dir)
+                        j = j+1
+                    
+                    labelType = labelType + 1
+
+                # Save the translated images.
+                # for fk in x_fake_list:
+                #     print(fk)
+                    
+                x_concat = torch.cat(x_fake_list, dim=3)
+                result_path = os.path.join(self.result_dir, '{}-images.jpg'.format(i+1))
+                save_image(self.denormalize(x_concat.data.cpu()), result_path, nrow=1, padding=0)
+                print('Saved real and fake images into {}...'.format(result_path))
+
+   
